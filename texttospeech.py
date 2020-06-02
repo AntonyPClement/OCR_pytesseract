75893aa20f4b84af7686cf8ebf0e621df8bc02a2
Vrishab Srivatsa <vsrivatsa25@gmail.com>
2020-06-02 23:05:01 +0530

    Add files via upload

--- texttospeech.py
+++ texttospeech.py
@@ -3,6 +3,8 @@ import pyttsx3
 engine = pyttsx3.init()
 
 rate = engine.getProperty('rate')
+voices = engine.getProperty('voices')
+engine.setProperty('rate', 125)
 engine.setProperty('voice', voices[1].id)
 def tts(string):
     engine.say(string)
