e39947b8ae7cab37a8a618172e8dc38763a83d8e
Vrishab Srivatsa <vsrivatsa25@gmail.com>
2020-06-02 23:02:51 +0530

    Update texttospeech.py

--- texttospeech.py
+++ texttospeech.py
@@ -4,7 +4,7 @@ engine = pyttsx3.init()
 
 rate = engine.getProperty('rate')
 engine.setProperty('voice', voices[1].id)
-def ttx(string):
+def tts(string):
     engine.say(string)
     engine.runAndWait()
 
